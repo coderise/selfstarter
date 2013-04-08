c974f9c6ff0c019bac8305aeac660b2e21f1672b
Elizabeth R <eramirem@gmail.com>
2013-04-08 14:59:57 -0400

    Email variables

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,6 +3,8 @@ class Notifier < ActionMailer::Base
 
   # send a signup email to the user, pass in the user object that contains the user's email address
   def donate_email(user, payment_option)
+  	@user = user
+  	@payment_option = payment_option
     mail( :to => user.email,
     :subject => "Your contribution to Coderise" )
   end
