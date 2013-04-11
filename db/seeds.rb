75acb88d6235f4631d85bc3dcd9d9693b1606be6
Elizabeth R <eramirem@gmail.com>
2013-04-11 11:04:35 -0400

    Gratitude payment option

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,13 @@
 PaymentOption.create(
     [
         {
+            amount_display: '$',
+            description: '<strong>Gratitude: </strong>We appreciate every donation! We will make sure you receive a shoutout on social media channels',
+            shipping_desc: '',
+            delivery_desc: '',
+            limit: -1
+        },
+        {
             amount: 25.00,
             amount_display: '$25',
             description: '<strong>Friend: </strong>Receive a heartfelt mention in our launch newsletter + a shoutout on social media channels on Coderise launch day!',
@@ -21,7 +28,7 @@ PaymentOption.create(
             description: '<strong>Supporter: </strong>Receive a personalized Coderise group photo + a mention in our social media channels.',
             shipping_desc: '',
             delivery_desc: '',
-            limit: 250
+            limit: -1
         },
         {
             amount: 100.00,
