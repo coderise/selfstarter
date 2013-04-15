cb9e9d22799e9d8450f5d7974b826cf0b9656ecd
Elizabeth R <eramirem@gmail.com>
2013-04-15 09:49:09 -0400

    Fix to email amount

--- app/controllers/preorder_controller.rb
+++ app/controllers/preorder_controller.rb
@@ -16,7 +16,13 @@ class PreorderController < ApplicationController
       payment_option_id = params[:payment_option]
       raise Exception.new("No payment option was selected") if payment_option_id.nil?
       payment_option = PaymentOption.find(payment_option_id)
-      price = payment_option.amount.to_f == 0.0 ? price = params[:amt_gross].to_f : payment_option.amount
+      #price = payment_option.amount.to_f == 0.0 ? price = params[:amt_gross].to_f : payment_option.amount
+      if payment_option.amount.to_f == 0.0
+        price = params[:amt_gross].to_f
+        payment_option.amount = price
+      else
+        price = payment_option.amount
+      end
     else
       price = Settings.price
     end
